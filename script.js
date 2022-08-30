de062dd15105ff47dd18d7f8f30cb995ae2602e7
Second <soltany.2019@gmail.com>
2022-08-30 15:35:13 +0430

    add script

---
+++ script.js
@@ -0,0 +1,45 @@
+// https://api.openweathermap.org/data/2.5/weather?q=tehran&appid=163b6c4d4627e9e8af6b29bc82b52ed9&units=metric
+
+const form = document.querySelector('.top-banner form');
+const input = document.querySelector('.top-banner input');
+const msg = document.querySelector('.top-banner .msg');
+const list = document.querySelector('.ajax-section .cities');
+
+
+const apiKey = '163b6c4d4627e9e8af6b29bc82b52ed9';
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    let inputVal = input.value;
+    // console.log(inputVal);
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            const { main, name, sys, weather } = data;
+            const icon = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${weather[0]["icon"]}.svg`;
+            const li = document.createElement('li');
+            li.classList.add('city');
+            const markup = `
+            <h2 class='city-name' data-name=${name},${sys.country}>
+                <span>${name}</span>
+                <span>${sys.country}</span>
+            </h2>
+            <div class='city-temp'>${Math.round(main.temp)}</div>
+            <figure>
+                <img class="city-icon" src="${icon}" alt="city">
+                <figurecaption>${weather[0]["description"]}</figurecaption>
+            </figure>
+            `;
+            li.innerHTML = markup;
+            list.appendChild(li);
+            msg.innerText = "";
+        })
+        .catch(() => {
+            msg.innerText = "search for a valid city"
+        })
+    input.value = "";
+})
+
+
